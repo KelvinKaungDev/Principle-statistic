6ee2aaa13e240272795c3b7feae90a58e90dc53a
Kaung Htet Oo <kaunghtetoo.dev@gmail.com>
2022-06-28 22:50:53 +0700

    Last Updated

--- Uniform/uniform_distribution_ans01.py
+++ Uniform/uniform_distribution_ans01.py
@@ -1,9 +1,9 @@
-from scipy.stats import norm
+from scipy.stats import uniform
 
-ans_one      = norm.cdf(6, 2, 6)
-ans_two      = 1 - norm.cdf(5.3, 2, 6)
-first_group  = norm.cdf(3, 2, 6)
-second_group = norm.cdf(7, 2, 6)
+ans_one      = uniform.cdf(6, 2, 6)
+ans_two      = 1 - uniform.cdf(5.3, 2, 6)
+first_group  = uniform.cdf(3, 2, 6)
+second_group = uniform.cdf(7, 2, 6)
 ans_three    = second_group - first_group
 
 print(ans_one)
--- Uniform/uniform_distribution_ans02.py
+++ Uniform/uniform_distribution_ans02.py
@@ -1,9 +1,9 @@
-from scipy.stats import norm
+from scipy.stats import uniform
 
-ans_one     = 1 - norm.cdf(30, 20, 40)
-ans_two     = 1 - norm.cdf(40, 20, 40)
-first_file  = norm.cdf(35, 20, 40)
-second_file = norm.cdf(45, 20, 40)
+ans_one     = uniform.cdf(30, 20, 40)
+ans_two     = 1 - uniform.cdf(40, 20, 40)
+first_file  = uniform.cdf(35, 20, 40)
+second_file = uniform.cdf(45, 20, 40)
 ans_three   = second_file - first_file
 
 print(ans_one)
